7e064ff4ff37945c30cd8a2d3d399561e6db194d
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-14 14:04:08 -0400

    Adding new fields to redux is 2 steps and EZAF!

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'The 80/20 Rule'},
-    {title: 'Steve Jobs Biography'},
-    {title: 'Ray Dalio: Principles'},
-    {title: 'Lean Startup'}
+    {title: 'The 80/20 Rule', pages: 101},
+    {title: 'Steve Jobs Biography', pages: 549},
+    {title: 'Ray Dalio: Principles', pages: 321},
+    {title: 'Lean Startup', pages: 201}
   ]
 }
